4b0c8e5224ab85b9885d258bcbd6f6e72fd645c9
ad-fleming <adfleming@gmail.com>
2020-09-30 21:11:25 -0400

    added route for notes.html

--- Develop/server.js
+++ Develop/server.js
@@ -19,6 +19,10 @@ app.get("/", function(req, res){
     res.sendFile(path.join(__dirname, "index.html"))
 })
 
+app.get("/notes", function(req, res){
+    res.sendFile(path.join(__dirname, "notes.html"))
+})
+
 
 // Start server to begin listening
 app.listen(PORT, function() {
